6411e3053bfcbf39521d01b09e0f92915fb5b8e2
bravorcarlos <bravorcarlos1@gmail.com>
2024-06-13 16:35:26 -0400

    Settings changed

--- marketplace/urls.py
+++ marketplace/urls.py
@@ -17,7 +17,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -32,5 +31,3 @@ urlpatterns = [
 if settings.DEBUG:
     from django.conf.urls.static import static
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-else:
-    urlpatterns += staticfiles_urlpatterns
